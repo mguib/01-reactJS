32419a48c15c824776cd26cd4a9d1525dee04a1b
Marcos Guibson <mgss.lic17@uea.edu.br>
2022-07-28 10:27:13 -0400

    fix: Modificando titulo do head component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header(){
     return(
         <header className={styles.header}>
             <img src={igniteLogo} alt="Logotipo do Ignite" />
-            <strong>Guibson Teste Feed</strong>
+            <strong>Ignite Feed</strong>
         </header>
         
     );
